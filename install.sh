71ebf802dc5b8ec364d22cd1b3798def7797ecc7
sletteon <misi.g126@gmail.com>
2018-01-27 18:50:43 +0100

    minden

---
+++ install.sh
@@ -0,0 +1,8 @@
+chmod +x motd.sh
+cp motd.sh /etc/profile.d/motd.sh
+
+echo '[+] motd.sh hozzáadva'
+
+cp motd /etc/motd
+
+echo '[+] motd kicserélve'
---
+++ motd.sh
@@ -0,0 +1,10 @@
+# IP-cím
+IP="$(hostname -I)"
+echo "//// ${IP}////"
+
+# uptime
+UP="$(uptime -p)"
+echo "//// ${UP} ////"
+
+#OUTPUT="$(ls -1)"
+#echo "${OUTPUT}"
